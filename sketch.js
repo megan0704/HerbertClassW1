bbaa24e78f32a81e6416be102ba866218560189d
megan0704 <401147277@qq.com>
2018-11-20 16:21:10 +0800

    updates basic files

---
+++ sketch.js
@@ -0,0 +1,21 @@
+// Move your finger across the page
+// to change its value
+var value = 0;
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+// function touchMoved() {
+//   value = value + 5;
+//   if (value > 255) {
+//     value = 0;
+//   }
+// }
+function touchStarted() {
+  if (value === 0) {
+    value = 255; }
+  else { value = 0;
+       }
+}
+// function touchStarted() {
+//   ellipse(mouseX, mouse
