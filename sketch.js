b0143f1416a3da4d9668091f1a492937efdda7fa
megan0704 <401147277@qq.com>
2018-12-15 18:04:51 +0800

    3

--- sketch.js
+++ sketch.js
@@ -33,14 +33,24 @@ var x=200;
 var y=200
 var r=50;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
+	background(100);
+   frameRate(100);
+   smooth();
 }
 
 function draw() {
-  background(220);
+
+	fill(0,5);
+		rect(0, 0, 1000, 1000);
   if(mouseIsPressed&&dist(mouseX,mouseY,x,y)<r){
   	x=mouseX;
     y=mouseY;
+
+		noStroke();  //lineweight);
+	  fill(350, 250, 250 );
+	ellipse(x,y, 30, 30) ;
   }
-  ellipse(x,y,r,r);
+
 }
